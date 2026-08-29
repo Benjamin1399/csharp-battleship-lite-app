c560b6f8e4b097e6e0011d57d8de6ea92e0b9a1d
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-08-29 22:48:36 +0200

    Added properties for GridModel

--- BattleshipLibrary/Models/GridSpotModel.cs
+++ BattleshipLibrary/Models/GridSpotModel.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace BattleshipLibrary.Models
 {
+    public enum Status
+    {
+        EMPTY,
+        POPULATED,
+        MISSED,
+        HIT
+    }
+
     public class GridSpotModel
     {
+        public string GridLetter { get; set; }
+
+        public int GridNumber { get; set; }
+
+        public Status GridStatus { get; set; }
     }
 }
