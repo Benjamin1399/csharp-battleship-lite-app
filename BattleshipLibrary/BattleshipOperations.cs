d220d727c3f3103be366ccbd9eaaff9561670d7a
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-08-30 20:25:31 +0200

    Created class for ConsoleUI operations

--- BattleshipLibrary/BattleshipOperations.cs
+++ BattleshipLibrary/BattleshipOperations.cs
@@ -1,12 +1,121 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BattleshipLibrary.Models;
 
 namespace BattleshipLibrary
 {
-    public class BattleshipOperations
+    public static class BattleshipOperations
     {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Battleship Lite App");
+            Console.WriteLine("**********************************");
+        }
+
+        public static string GetUserName(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string GetGridLetter(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetGridNumber(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int GetIntFromUser(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public static void StoreUserName(UserModel user, string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void GetShipPlacements(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsStoreSpotValid(UserModel user, string gridLetter, int gridNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void StoreShip(UserModel user, string gridLetter, int gridNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void StoreShot(UserModel user, string gridLetter, int gridNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void CreateStoredShipGrid(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void CreateShotShipGrid(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DisplayStoredShipGrid(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DisplayShotShipGrid(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void FireOnSpot(UserModel user, string gridLetter, int gridNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ValidateShot(UserModel user, string gridLetter, int gridNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ResultOfShot(UserModel user, string gridLetter, int gridNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DisplayScore(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DetermineWinner(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void DisplayStats(UserModel user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SetupPlayerInfo(UserModel user, string message)
+        {
+            string userName = GetUserName(message);
+            GetShipPlacements(user);
+        }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BattleshipLibrary;
 using BattleshipLibrary.Models;
 
 namespace ConsoleUI
@@ -11,6 +12,12 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            UserModel player1 = new UserModel();
+
+            BattleshipOperations.WelcomeMessage();
+            BattleshipOperations.SetupPlayerInfo(user, "Enter username for player 1: ");
+            BattleshipOperations.SetupPlayerInfo(user, "Enter username for player 2: ");
+
         }
     }
 }
